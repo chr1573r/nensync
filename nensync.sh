a7fc72aaef761638d5f2b7ad81d2a2913b20db2e
Christer Jonassen <christer@csdnserver.com>
2012-04-06 23:36:23 +0200

    Removed test line

--- nensync.sh
+++ nensync.sh
@@ -72,7 +72,6 @@ gfx ()
 			echo
 			echo
 			echo -e "$GREEN""          nensync version $VERSION"$DEF" - " $BLUE"Nordic Encryption Net"$DEF" (C) "$BLUE"2011-2012"$DEF
-			echo This is a test!
 			sleep 2
 			clear
 			
