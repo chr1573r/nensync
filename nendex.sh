4e163f24bf3efeb46bb7e82379c7c221ed3d80eb
Christer Jonassen <christer@csdnserver.com>
2012-07-24 04:37:38 +0200

    Major rewrite of nendex.sh
    nendex 1.0.0:
    - Not broken anymore!
    - Creates a filelisting containing unique hash value and the full path of each (non-empty) file and saves it to the file nendex.db
    - Reads nendata dir from server.cfg file, not hard-coded like before.
    - Uses the slightly faster SHA512 hashing algorithm instead of the previously used MD5
    - Prettier UI, displays current time for each operation and summerizes total elapsed time at the end (courtesy of http://www.linuxjournal.com/content/use-date-command-measure-elapsed-time)
    
    Current limitations:
    - Rebuilds the whole db everytime, no incremental function implemented yet.
    - It would be nice if the summary also said something about how many files that were indexed and the average file indexing time
    - No data retrieving/manipulation of nendex.db is implemented, it can only generate the db.

--- nendex.sh
+++ nendex.sh
@@ -1,12 +1,96 @@
 #!/bin/bash
 
 # nendex.sh
-# Creates a file index and calculates the MD5 of each file
-# v. 0.0.1
+# Creates a file index and calculates the SHA512 of each file in the nen data directory
+VERSION="1.0.0"
 
-echo nendex - started `date '+%d.%m.%Y, %H:%M'`
+# Pretty colours!
+DEF="\x1b[0m"
+WHITE="\e[0;37m"
+BLUE="\x1b[34;01m"
+CYAN="\x1b[36;01m"
+GREEN="\x1b[32;01m"
+RED="\x1b[31;01m"
+GRAY="\x1b[37;01m"
+YELLOW="\x1b[33;01m"
 
-	
+
+
+function timer()
+# timer function (http://www.linuxjournal.com/content/use-date-command-measure-elapsed-time)
+# Elapsed time.  Usage:
+#
+#   t=$(timer)
+#   ... # do something
+#   printf 'Elapsed time: %s\n' $(timer $t)
+#      ===> Elapsed time: 0:01:12
+#
+#
+#####################################################################
+# If called with no arguments a new timer is returned.
+# If called with arguments the first is used as a timer
+# value and the elapsed time is returned in the form HH:MM:SS.
+#
+{
+    if [[ $# -eq 0 ]]; then
+        echo $(date '+%s')
+    else
+        local  stime=$1
+        etime=$(date '+%s')
+
+        if [[ -z "$stime" ]]; then stime=$etime; fi
+
+        dt=$((etime - stime))
+        ds=$((dt % 60))
+        dm=$(((dt / 60) % 60))
+        dh=$((dt / 3600))
+        printf '%d:%02d:%02d' $dh $dm $ds
+    fi
+}
+
+# If invoked directly run test code.
+if [[ $(basename $0 .sh) == 'timer' ]]; then
+    t=$(timer)
+    read -p 'Enter when ready...' p
+    printf 'Elapsed time: %s\n' $(timer $t)
+fi
+
+
+
+
+# Start timer, set start time, read settings from server.cfg
+t=$(timer)
+STARTTIME=`date '+%d.%m.%Y, %H:%M'`
+for i in `cat server.cfg`; do export "$i"; done 2>/dev/null
+
+# Erase existing nendex.db content
+#
+# Hopefully, nendex will be capable of incremential updates to the db, so it won't need to be rebuilt all the time from scratch
+echo "#nendex.db made with nendex $VERSION">./nendex.db
+echo >>./nendex.db
+
+# GUI init
+clear
+# Header
+			echo -e "$RED""///"$YELLOW" nendex.sh v.$VERSION "$RED"/// "$DEF""
+echo
 echo "Updating nendex...(datadir: $REMOTEPATH)"
-find ./data/* -type f -print0 | xargs -0 md5sum > ./nendex.md5 
-echo Last updated: `date '+%d.%m.%Y, %H:%M'`>>./nendex.md5 
+echo
+echo -e "["$YELLOW"nendex"$DEF"] initialized at `date '+%d.%m.%Y, %H:%M'`"
+# for each file found in .data that is not empty or a directory, do:
+find $REMOTEPATH -not -empty -type f -print0 | xargs -0 ls | while read CURRENTFILE;
+do
+	# Tell which file is currently being hashed to console (STDOUT)
+	echo -e "["$YELLOW"nendex "$GREEN"#"$DEF" `date '+%H:%M'`] Currently processing file "$CYAN"$CURRENTFILE"$DEF"..."
+	# Generate SHA512 hash of $CURRENTFILE and write to file nendex.db
+	sha512sum "$CURRENTFILE" >>./nendex.db
+done
+
+# Add timestamp to .db
+echo >./nendex.db
+echo Last updated: `date '+%d.%m.%Y, %H:%M'`>>./nendex.db
+
+# Report to gui, quit
+echo "nendex update finished at `date '+%d.%m.%Y, %H:%M'`"
+printf 'Elapsed time: %s\n' $(timer $t)
+
