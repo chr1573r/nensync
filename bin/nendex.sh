63707facf01dc56f3d17abd31517ef6cc70d88ff
Christer Jonassen <christer@csdnserver.com>
2012-10-16 11:51:18 +0200

    Made a few compatibility changes to nendex.sh The whole thing needs a re-write though

--- bin/nendex.sh
+++ bin/nendex.sh
@@ -2,10 +2,16 @@
 
 # nendex.sh
 # Creates a file index and calculates the SHA512 of each file in the nen data directory
-APPVERSION="1.1.1"
+APPVERSION="1.2.0"
 
-# Load nenlib
-source nenlib.sh
+# Get directory where nendex.sh is stored
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Read variables from cfg file
+source $DIR/../cfg/nen.cfg
+
+# We need to load functions fron nenlib.sh:
+source $NENDIR/bin/nenlib.sh
 
 # Start timer, set start time, read settings from server.cfg
 t=$(timer)
@@ -15,29 +21,29 @@ for i in `cat server.cfg`; do export "$i"; done 2>/dev/null
 # Erase existing nendex.db content
 #
 # Hopefully, nendex will be capable of incremential updates to the db, so it won't need to be rebuilt all the time from scratch
-echo "#nendex.db made with nendex $APPVERSION">./nendex.db
-echo >>./nendex.db
+echo "#nendex.db made with nendex $APPVERSION">$NENDIR/sys/nendex.db
+echo >>$NENDIR/sys/nendex.db
 
 # GUI init
 clear
 # Header
 			echo -e "$RED""///"$YELLOW" nendex.sh v.$APPVERSION "$RED"/// "$DEF""
 echo
-echo "Updating nendex...(datadir: $REMOTEPATH)"
+echo "Updating nendex...(datadir: $DATADIR)"
 echo
 echo -e "["$YELLOW"nendex"$DEF"] initialized at `date '+%d.%m.%Y, %H:%M'`"
 # for each file found in .data that is not empty or a directory, do:
-find $REMOTEPATH -not -empty -type f -print0 | xargs -0 ls | while read CURRENTFILE;
+find $DATDIR -not -empty -type f -print0 | xargs -0 ls | while read CURRENTFILE;
 do
 	# Tell which file is currently being hashed to console (STDOUT)
 	echo -e "["$YELLOW"nendex "$GREEN"#"$DEF" `date '+%H:%M'`] Currently processing file "$CYAN"$CURRENTFILE"$DEF"..."
 	# Generate SHA512 hash of $CURRENTFILE and write to file nendex.db
-	sha512sum "$CURRENTFILE" >>./nendex.db
+	sha512sum "$CURRENTFILE" >>$NENDIR/sys/nendex.db
 done
 
 # Add timestamp to .db
-echo >>./nendex.db
-echo Last updated: `date '+%d.%m.%Y, %H:%M'`>>./nendex.db
+echo >>$NENDIR/sys/nendex.db
+echo Last updated: `date '+%d.%m.%Y, %H:%M'`>>$NENDIR/sys/nendex.db
 
 # Report to gui, quit
 echo "nendex update finished at `date '+%d.%m.%Y, %H:%M'`"
